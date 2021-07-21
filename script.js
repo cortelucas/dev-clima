4333e624d6a198e05869f49a7f8c116a6a5b231d
Lucas Corte <corte1994.lc@gmail.com>
2021-07-21 11:14:22 -0300

    implementação da função showInfo

--- script.js
+++ script.js
@@ -27,7 +27,17 @@ search.addEventListener('submit', async (e) => {
 });
 
 const showInfo = (json) => {
-  
+  showWarning('');
+
+  document.querySelector('.resultado').style.display = 'block';
+
+  document.querySelector('.titulo').innerHTML = `${json.name}, ${json.country}`;
+  document.querySelector('.tempInfo').innerHTML = `${json.temp} <sup>ºC</sup>`;
+  document.querySelector('.ventoInfo').innerHTML = `${json.windSpeed} <span></span>`
+
+  document.querySelector('.temp img').setAttribute('src', `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`);
+
+  document.querySelector('.ventoPonto').style.transform = `rotate(${json.windDeg - 90}deg)`;
 }
 
 const showWarning = () => {
