a7cbc9145a4c822273d1c0c3fbbe4999fb695550
Lucas Corte <corte1994.lc@gmail.com>
2021-07-21 11:24:58 -0300

    implementação da função clearInfo

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ const search = document.querySelector('.busca');
 search.addEventListener('submit', async (e) => {
   e.preventDefault();
 
-  const input = document.querySelector('.searchInput').ariaValueMax;
+  let input = document.querySelector('#searchInput').value;
 
   if(input !== '') {
+    clearInfo();
     showWarning('Carregando ...');
 
-    const url = `api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=49d36bba59b54549fcefe79d51fff261&units=metric&lang=pt_br`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=49d36bba59b54549fcefe79d51fff261&units=metric&lang=pt_br`;
     const results = await fetch(url);
     const json = await results.json();
     if(json.cod === 200) {
@@ -21,6 +22,7 @@ search.addEventListener('submit', async (e) => {
         windDeg: json.wind.deg
       });
     } else {
+      clearInfo();
       showWarning('Não encontramos esta localização!')
     }
   }
@@ -40,6 +42,11 @@ const showInfo = (json) => {
   document.querySelector('.ventoPonto').style.transform = `rotate(${json.windDeg - 90}deg)`;
 }
 
-const showWarning = () => {
+const clearInfo = () => {
+  showWarning('');
+  document.querySelector('.resultado').style.display = 'none';
+}
+
+const showWarning = (msg) => {
   document.querySelector('.aviso').innerHTML = msg;
 }
