7addd902f5fd6892f8eb252a6f65295e5d485d79
Lucas Corte <corte1994.lc@gmail.com>
2021-07-21 11:03:20 -0300

    implementação da lógica

--- script.js
+++ script.js
@@ -9,9 +9,26 @@ search.addEventListener('submit', async (e) => {
     showWarning('Carregando ...');
 
     const url = `api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=49d36bba59b54549fcefe79d51fff261&units=metric&lang=pt_br`;
-    
+    const results = await fetch(url);
+    const json = await results.json();
+    if(json.cod === 200) {
+      showInfo({
+        name: json.name,
+        country: json.sys.country,
+        temp: json.main.temp,
+        tempIcon: json.weather[0].icon,
+        windSpeed: json.wind.speed,
+        windDeg: json.wind.deg
+      });
+    } else {
+      showWarning('Não encontramos esta localização!')
+    }
   }
 });
+
+const showInfo = (json) => {
+  
+}
 
 const showWarning = () => {
   document.querySelector('.aviso').innerHTML = msg;
